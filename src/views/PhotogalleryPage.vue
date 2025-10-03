12aec007daddb749292befe130be0e7303e0421a
mpedone <mpedone@contents.com>
2025-10-03 14:38:50 +0200

    add fix

--- src/views/PhotogalleryPage.vue
+++ src/views/PhotogalleryPage.vue
@@ -2,13 +2,13 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>MP's Photo Gallery</ion-title>
+        <ion-title>MP's PhotoGallery</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Photo Gallery</ion-title>
+          <ion-title size="large">PhotoGallery</ion-title>
         </ion-toolbar>
       </ion-header>
       <ion-grid>
